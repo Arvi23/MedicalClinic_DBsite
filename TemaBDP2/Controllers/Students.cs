ee4501e50bfaacda8bda361fb1142f6e4d3a753b
Vasile Radu <carpatirtg@yahoo.ro>
2024-03-15 19:36:59 +0200

    Commit

---
+++ TemaBDP2/Controllers/Students.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemaBD.Models
+{
+    public class Students : Controller
+    {
+        public string Index(string name, string? id)
+        {
+            string response = "Hello " + name + " ";
+            if (id != null)
+            {
+                response = response + "id = " + id;
+            }
+            return response;
+            //return View();
+        }
+        public string Coaie(string id)
+        {
+            string response = "Ai scris";
+            if (id != null)
+                response = response + " id = " + id;
+            else
+                response = response + " nimic";
+            return response;
+        }
+
+
+    }
+}
---
+++ TemaBDP2/Data/ClinicDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace TemaBD.Models;
+
+public class ClinicDbContext : DbContext
+{
+    public ClinicDbContext(DbContextOptions<ClinicDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Pacient> Pacienti { get; set; }
+    public DbSet<Consultatie> Consultatii { get; set; }
+    public DbSet<Medic> Medici { get; set; }
+    public DbSet<Serviciu> Servicii { get; set; }
+    public DbSet<DetaliiConsultatie> DetaliiConsultatii { get; set; }
+}
